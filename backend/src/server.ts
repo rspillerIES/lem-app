c9ce5e79bffc1620997eb2c09f84067689b64684
rspillerIES <rspiller@impact-energy.ca>
2026-04-28 11:03:56 -0600

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -71,6 +71,16 @@ app.use(errorHandler);
 /**
  * Start server
  */
+// Catch uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
 export function startServer() {
   app.listen(PORT, () => {
     console.log(`
